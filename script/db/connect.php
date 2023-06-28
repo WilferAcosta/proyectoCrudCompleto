0bb41bb9a549b1bd27336d65b90b6e44d30bdd0c
WilferAcosta <w.o.a.r@hotmail.es>
2023-06-27 21:02:36 -0500

    feat: :bug: nada
    nada qeu se soluciona

--- script/db/connect.php
+++ script/db/connect.php
@@ -7,6 +7,8 @@ class connect {
             $this->conx = new \PDO(
                 $_ENV["DNS"].":host=".$_ENV["HOST"].";dbname=".$_ENV["DBNAME"].";user=".$_ENV["USER"].";password=".$_ENV["PASSWORD"].";port=".$_ENV["PORT"]
             );
+            $this->conx->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_SILENT);
+            $this->conx->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_WARNING);
             $this->conx->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         } catch (\PDOException $e) {
             print_r($e->getMessage());
